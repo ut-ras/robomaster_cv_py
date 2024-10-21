2930ad53c0f894c56b7ae0684aeb040619d37375
prajithv <prajith.velicheti@gmail.com>
2024-10-21 18:13:24 -0500

    Update to manual computation of lengths, widths, and angles

--- classical-filter/classical-filter.py
+++ classical-filter/classical-filter.py
@@ -46,8 +46,8 @@ def get_contours(frame):
 
     return contours
 
-def draw_centers(frame):
-    contours = get_contours(frame)
+def draw_centers(frame, color):
+    contours = get_contours(frame, color)
 
     # Compute rotated bounding box for each contour and store in `bboxes`
     bboxes = []
@@ -59,24 +59,36 @@ def draw_centers(frame):
         frame = cv.drawContours(frame, [bbox_points], -1, (0, 255, 0), 2)
 
     thresh = 70
-    width_sim_thresh, length_sim_thresh = 0.1, 0.4
-
+    width_sim_thresh, length_sim_thresh, angle_thresh = 0.1, 0.4, 5
     for i in range(len(bboxes)):
         bbox1 = bboxes[i]
         width1, length1, angle1 = bbox1[1][0], bbox1[1][1], bbox1[2]
+
+        vert1 = cv.boxPoints(bbox1)
+
+        longest = (0, 1)
+        bl = vert1[0]
+        tl = vert1[1]
+        br = vert1[3]
+        width1 = math.sqrt(dist_sq(vert1[0], vert1[3]))
+        if dist_sq(bl, br) > dist_sq(bl, tl):
+            longest = (0, 3)
+            width1 = math.sqrt(dist_sq(vert1[0], vert1[1]))
+        length1 = math.sqrt(dist_sq(vert1[longest[0]], vert1[longest[1]]))
+        angle1 = angle(vert1[longest[0]], vert1[longest[1]])
 
         # If the bounding box is too small, skip
         if max(length1, width1) < thresh:
             continue
 
         # how the heck is `minAreaRect` defining the angle
-        if angle1 > 45:
-            angle1 = 90 - angle1
-            width1, length1 = length1, width1
+        # if angle1 > 45:
+        #     angle1 = 90 - angle1
+        #     width1, length1 = length1, width1
 
         # Matching longer sides is more important, and thus needs a stricter threshold
-        if width1 > length1:
-            width_sim_thresh, length_sim_thresh = length_sim_thresh, width_sim_thresh
+        # if width1 > length1:
+        #     width_sim_thresh, length_sim_thresh = length_sim_thresh, width_sim_thresh
 
         for j in range(i + 1, len(bboxes)):
             bbox2 = bboxes[j]
@@ -85,15 +97,29 @@ def draw_centers(frame):
             if max(length2, width2) < thresh:
                 continue
 
-            if angle2 > 45:
-                angle2 = 90 - angle2
-                width2, length2 = length2, width2
+            vert2 = cv.boxPoints(bbox2)
+            
+            longest = (0, 1)
+            bl = vert2[0]
+            tl = vert2[1]
+            br = vert2[3]
+            width2 = math.sqrt(dist_sq(vert2[0], vert2[3]))
+            if dist_sq(bl, br) > dist_sq(bl, tl):
+                longest = (0, 3)
+                width2 = math.sqrt(dist_sq(vert2[0], vert2[1]))
+            length2 = math.sqrt(dist_sq(vert2[longest[0]], vert2[longest[1]]))
+            angle2 = angle(vert2[longest[0]], vert2[longest[1]])
+
+            # if angle2 > 45:
+            #     angle2 = 90 - angle2
+            #     width2, length2 = length2, width2
 
             angle_diff = abs(angle1 - angle2)
 
             # If two bounding boxes are similar in size and orientation, place a dot between them
-            if width_sim_thresh < sim(width1, width2) and length_sim_thresh < sim(length1, length2) and angle_diff < 5:
+            if width_sim_thresh < sim(width1, width2) and length_sim_thresh < sim(length1, length2) and angle_diff < angle_thresh:
                 cv.circle(frame, (round((bbox1[0][0] + bbox2[0][0]) / 2), round((bbox1[0][1] + bbox2[0][1]) / 2)), 10, (255, 0, 255), -1)
+            cv.putText(frame, f'w: {sim(width1, width2):.2f}, l: {sim(length1, length2):.2f}, a1: {angle1:.2f}, a2: {angle2:.2f}, {angle_diff:.2f}', (round((bbox1[0][0] + bbox2[0][0]) / 2), round((bbox1[0][1] + bbox2[0][1]) / 2)), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1, cv.LINE_AA)
 
             # Debugging
             # print(f'w --- {i}: {width1}, {j}: {width2}, sim: {sim(width1, width2)}')
@@ -106,7 +132,13 @@ def draw_centers(frame):
     return frame
 
 def main():
-    cap = cv.VideoCapture('IMG_2129.MOV')
+    cap = cv.VideoCapture('tests/IMG_7755.MOV')
+
+    output = 'output.mov'
+    # fourcc = cv.VideoWriter_fourcc(*'mp4v')
+    # width = cap.get(cv.CAP_PROP_FRAME_WIDTH)
+    # height = cap.get(cv.CAP_PROP_FRAME_HEIGHT)
+    # writer = cv.VideoWriter(output, fourcc, 60.0, (640, 180))
 
     while cap.isOpened():
         # start_time = time.time()
@@ -117,14 +149,17 @@ def main():
             print('Failed to read frame. Exiting...')
             break
 
-        frame = draw_centers(frame)
+        frame = draw_centers(frame, 'red')
 
         cv.imshow('frame', frame)
+
+        # writer.write(frame)
+        # cv.imwrite(output, frame)
 
         # end_time = time.time()
         # print(end_time - start_time)
 
-        if cv.waitKey(1) == ord('q'):
+        if cv.waitKey(2) == ord('q'):
             break
 
     cap.release()
